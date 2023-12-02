29dc1cab7424ecb1f03ee06eccab006bb37d8743
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-02 16:43:45 +1100

    Day 2, part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,3 +1,5 @@
+use std::cmp::{max, min};
+
 use itertools::Itertools;
 use regex::Regex;
 
@@ -46,7 +48,35 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    Some(
+        input
+            .split('\n')
+            .filter_map(|line| {
+                let (game, result) = line.split(':').collect_tuple()?;
+                let game_id: u32 = game[5..].parse().unwrap();
+                let (mut min_red, mut min_green, mut min_blue) = (0, 0, 0);
+                for draw in result.split(";") {
+                    let re = Regex::new(r"([0-9]+) (red|green|blue)").unwrap();
+                    for c in re.captures_iter(draw) {
+                        let count: u32 = c.get(1).unwrap().as_str().parse().unwrap();
+                        match c.get(2)?.as_str() {
+                            "red" => min_red = max(count, min_red),
+                            "green" => min_green = max(count, min_green),
+                            "blue" => min_blue = max(count, min_blue),
+                            _ => (),
+                        }
+                    }
+                }
+
+                let power = min_red * min_green * min_blue;
+                println!(
+                    "game {game_id}, red {min_red}, green {min_green}, blue {min_blue}, {power}"
+                );
+
+                Some(power)
+            })
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -62,6 +92,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2286));
     }
 }
