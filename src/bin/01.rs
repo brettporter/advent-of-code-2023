1e717e25d7d106ba38b950f7a2eb3baf8f611bb7
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-02 14:11:29 +1100

    Failed attempt at day 1, part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -13,7 +15,34 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let digits = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    let new_input = input
+        .split('\n')
+        .map(|line| {
+            let mut new_line = line.to_string();
+
+            loop {
+                match digits
+                    .iter()
+                    .enumerate()
+                    .filter_map(|(to, from)| new_line.find(*from).map(|v| (from, to + 1, v)))
+                    .min_by_key(|x| x.2)
+                {
+                    Some(sub) => {
+                        new_line = new_line.replacen(sub.0, sub.1.to_string().as_str(), 1);
+                    }
+                    None => break,
+                };
+            }
+
+            new_line
+        })
+        .join("\n");
+
+    part_one(&new_input)
 }
 
 #[cfg(test)]
@@ -28,7 +57,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result.unwrap(), 281);
     }
 }
