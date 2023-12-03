1d0c60ddf410a43a4b77137cdfb890702f3cedc4
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-03 21:25:20 +1100

    refactor common code

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -5,68 +5,59 @@ use regex::Regex;
 
 advent_of_code::solution!(3);
 
-pub fn part_one(input: &str) -> Option<u32> {
+fn read_engine_schema<F>(input: &str, mut process: F)
+where
+    F: FnMut(u8, (i32, i32), &str, (usize, usize)),
+{
     let re = Regex::new(r"(\d+)").unwrap();
     let lines = input.trim().split('\n').collect_vec();
-    let mut values: Vec<u32> = Vec::new();
     for row in 0..lines.len() {
         let line = lines[row];
         for cap in re.captures_iter(line) {
             let v = cap.get(1).unwrap();
-            let mut valid = false;
             for y in row as i32 - 1..=row as i32 + 1 {
                 if y >= 0 && y < lines.len() as i32 {
                     for x in v.start() as i32 - 1..v.end() as i32 + 1 {
                         if x >= 0 && x < line.len() as i32 {
                             let c = lines[y as usize].as_bytes()[x as usize];
-                            if c != b'.' && (c < b'0' || c > b'9') {
-                                valid = true;
-                            }
+                            process(c, (x, y), v.as_str(), (v.start(), row));
                         }
                     }
                 }
             }
-            if valid {
-                values.push(v.as_str().parse().unwrap());
-            }
         }
     }
+}
 
-    Some(values.iter().sum())
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut parts = HashMap::new();
+    read_engine_schema(input, |c, _, v, key| {
+        if c != b'.' && (c < b'0' || c > b'9') {
+            parts.insert(key, v.parse().unwrap());
+        }
+    });
+
+    Some(parts.values().sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let re = Regex::new(r"(\d+)").unwrap();
-    let lines = input.trim().split('\n').collect_vec();
-    let mut gears: HashMap<(i32, i32), Vec<&str>> = HashMap::new();
-    for row in 0..lines.len() {
-        let line = lines[row];
-        for cap in re.captures_iter(line) {
-            let v = cap.get(1).unwrap();
-            for y in row as i32 - 1..=row as i32 + 1 {
-                if y >= 0 && y < lines.len() as i32 {
-                    for x in v.start() as i32 - 1..v.end() as i32 + 1 {
-                        if x >= 0 && x < line.len() as i32 {
-                            let c = lines[y as usize].as_bytes()[x as usize];
-                            if c == b'*' {
-                                let key = (x, y);
-                                if let Some(previous) = gears.get_mut(&key) {
-                                    previous.push(v.as_str());
-                                } else {
-                                    gears.insert(key, vec![v.as_str()]);
-                                }
-                            }
-                        }
-                    }
-                }
+    let mut gears: HashMap<(i32, i32), Vec<u32>> = HashMap::new();
+
+    read_engine_schema(input, |c, key, v, _| {
+        if c == b'*' {
+            if let Some(previous) = gears.get_mut(&key) {
+                previous.push(v.parse::<u32>().unwrap());
+            } else {
+                gears.insert(key, vec![v.parse::<u32>().unwrap()]);
             }
         }
-    }
+    });
+
     Some(
         gears
             .values()
             .filter(|g| g.len() == 2)
-            .map(|g| g[0].parse::<u32>().unwrap() * g[1].parse::<u32>().unwrap())
+            .map(|g| g[0] * g[1])
             .sum(),
     )
 }
