499e53ed73673d9bb06041868db5adc9e887137b
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-03 22:50:08 +1100

    add comments

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -13,8 +13,10 @@ where
     let lines = input.trim().split('\n').collect_vec();
     for row in 0..lines.len() {
         let line = lines[row];
+        // Get all the numbers in the line
         for cap in re.captures_iter(line) {
             let v = cap.get(1).unwrap();
+            // Search the box surrounding the number, with bounds checking
             for y in row as i32 - 1..=row as i32 + 1 {
                 if y >= 0 && y < lines.len() as i32 {
                     for x in v.start() as i32 - 1..v.end() as i32 + 1 {
@@ -32,7 +34,9 @@ where
 pub fn part_one(input: &str) -> Option<u32> {
     let mut parts = HashMap::new();
     read_engine_schema(input, |c, _, v, key| {
+        // if the coordinate is a symbol (in this case, not a . or 0-9), add it to the list of parts
         if c != b'.' && (c < b'0' || c > b'9') {
+            // add to hashmap with a key for that coordinate to avoid duplicates on that number
             parts.insert(key, v.parse().unwrap());
         }
     });
@@ -44,7 +48,9 @@ pub fn part_two(input: &str) -> Option<u32> {
     let mut gears: HashMap<(i32, i32), Vec<u32>> = HashMap::new();
 
     read_engine_schema(input, |c, key, v, _| {
+        // if the coordinate is '*', process it as a gear
         if c == b'*' {
+            // Append the number to the list of numbers for that gear (creating the list if none found yet)
             if let Some(previous) = gears.get_mut(&key) {
                 previous.push(v.parse::<u32>().unwrap());
             } else {
@@ -53,6 +59,7 @@ pub fn part_two(input: &str) -> Option<u32> {
         }
     });
 
+    // For gears with exactly two part numbers, multiply them and then sum the total
     Some(
         gears
             .values()
