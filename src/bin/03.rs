d98d32d701cfa7afc8ce49a6ce91c741bab7dd19
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-03 20:06:28 +1100

    Day 3, part 2 first submission

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use regex::Regex;
 
@@ -17,7 +19,7 @@ pub fn part_one(input: &str) -> Option<u32> {
                     for x in v.start() as i32 - 1..v.end() as i32 + 1 {
                         if x >= 0 && x < line.len() as i32 {
                             let c = lines[y as usize].as_bytes()[x as usize];
-                            if c != 46 && (c < 48 || c > 57) {
+                            if c != b'.' && (c < b'0' || c > b'9') {
                                 valid = true;
                             }
                         }
@@ -34,7 +36,39 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let re = Regex::new(r"(\d+)").unwrap();
+    let lines = input.trim().split('\n').collect_vec();
+    let mut gears: HashMap<(i32, i32), Vec<&str>> = HashMap::new();
+    for row in 0..lines.len() {
+        let line = lines[row];
+        for cap in re.captures_iter(line) {
+            let v = cap.get(1).unwrap();
+            for y in row as i32 - 1..=row as i32 + 1 {
+                if y >= 0 && y < lines.len() as i32 {
+                    for x in v.start() as i32 - 1..v.end() as i32 + 1 {
+                        if x >= 0 && x < line.len() as i32 {
+                            let c = lines[y as usize].as_bytes()[x as usize];
+                            if c == b'*' {
+                                let key = (x, y);
+                                if let Some(previous) = gears.get_mut(&key) {
+                                    previous.push(v.as_str());
+                                } else {
+                                    gears.insert(key, vec![v.as_str()]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Some(
+        gears
+            .values()
+            .filter(|g| g.len() == 2)
+            .map(|g| g[0].parse::<u32>().unwrap() * g[1].parse::<u32>().unwrap())
+            .sum(),
+    )
 }
 
 #[cfg(test)]
@@ -50,6 +84,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(467835));
     }
 }
