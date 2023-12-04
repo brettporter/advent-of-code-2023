3a4294415ff3797f828064678097b143e1028c78
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-04 19:08:00 +1100

    Day 4, part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,37 +1,50 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 
 advent_of_code::solution!(4);
 
 fn score_card(winning_numbers: Vec<i32>, card_numbers: Vec<i32>) -> u32 {
-    let winning_numbers = card_numbers
-        .iter()
-        .filter(|v| winning_numbers.contains(v))
-        .count();
+    let winning_numbers = count_winning_numbers(winning_numbers, card_numbers);
+    // 1 point for first number, then double for every after that = 2 ^ (num - 1)
     if winning_numbers > 0 {
-        2_u32.pow(winning_numbers as u32 - 1)
+        2_u32.pow(winning_numbers - 1)
     } else {
         0
     }
 }
 
+fn count_winning_numbers(winning_numbers: Vec<i32>, card_numbers: Vec<i32>) -> u32 {
+    card_numbers
+        .iter()
+        .filter(|v| winning_numbers.contains(v))
+        .count() as u32
+}
+
+fn parse_card(line: &str) -> (Vec<i32>, Vec<i32>) {
+    let (winning, card) = line.split(':').collect_vec()[1]
+        .split('|')
+        .collect_tuple()
+        .unwrap();
+    (
+        winning
+            .split_ascii_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect_vec(),
+        card.split_ascii_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect_vec(),
+    )
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    // Calculate the score for each card and then sum the total
     Some(
         input
             .trim()
             .split('\n')
             .map(|line| {
-                let (winning, card) = line.split(':').collect_vec()[1]
-                    .split('|')
-                    .collect_tuple()
-                    .unwrap();
-                let winning_numbers = winning
-                    .split_ascii_whitespace()
-                    .map(|n| n.parse().unwrap())
-                    .collect_vec();
-                let card_numbers = card
-                    .split_ascii_whitespace()
-                    .map(|n| n.parse().unwrap())
-                    .collect_vec();
+                let (winning_numbers, card_numbers) = parse_card(line);
                 score_card(winning_numbers, card_numbers)
             })
             .sum(),
@@ -39,7 +52,36 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    // Simplifying assumptions
+    // - input cards are in order
+    // - no cards extend beyond the list (per instructions)
+
+    // Use a map to keep track of the count of each card number
+    // (alternatively could use a vector with pre-determined size and index them since it's ordered and not sparse)
+    let mut card_count = HashMap::new();
+
+    for (i, card) in input.trim().split('\n').enumerate() {
+        let card_num = i + 1;
+
+        // Get the number of copies we already have of this card, including the original
+        let copies = *card_count
+            .entry(card_num)
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+
+        let (winning_numbers, card_numbers) = parse_card(card);
+        let c = count_winning_numbers(winning_numbers, card_numbers) as usize;
+        // For each of the following cards (up to the limit of winning numbers),
+        // add one for each copy we have
+        for j in 1..=c {
+            card_count
+                .entry(card_num + j)
+                .and_modify(|v| *v += copies)
+                .or_insert(copies);
+        }
+    }
+
+    Some(card_count.values().sum())
 }
 
 #[cfg(test)]
@@ -74,6 +116,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
