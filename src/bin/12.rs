cc964351f3c093f9006d97e1b5d59066fa17e64a
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-12 22:22:08 +1100

    improved day 12, part 2 but still too slow

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,91 +1,38 @@
+use std::cmp::min;
+
 use aoc_parse::{parser, prelude::*};
 
 advent_of_code::solution!(12);
 
 fn find_arrangements(str: &str, sizes: &[i32]) -> i32 {
-    let total = sizes.iter().sum::<i32>();
-    let l = str.len();
-    let missing = str.len() as i32 - sizes.iter().sum::<i32>();
-    let to_allocate = missing - (sizes.len() as i32 - 1);
-    let first = 0;
-    let last = sizes.len();    
+    find_arrangements_internal(str, sizes, 0)
+}
 
-    let mut combos = Vec::new();
-    for i in 0..=to_allocate {
-        combos.push(vec![i]);
-    }
-    for pos in 1..=last {
-        let mut new_combos = Vec::new();
-        for c in &combos {
-            let used = c.iter().sum::<i32>();
-            if pos < last {
-                for i in 0..=(to_allocate - used) {
-                    let mut new_c = c.clone();
-                    new_c.push(i);
-                    new_combos.push(new_c);
-                }
-            } else {
-                let mut new_c = c.clone();
-                new_c.push(to_allocate - used);
-                new_combos.push(new_c);
-            }
+fn find_arrangements_internal(str: &str, sizes: &[i32], min_space: usize) -> i32 {
+    if sizes.len() == 0 {
+        let b = str.as_bytes();
+        if b[..].iter().all(|c| *c == b'.' || *c == b'?') {
+            return 1;
+        } else {
+            return 0;
         }
-        combos = new_combos;
     }
 
-    // TODO: need to build the combos faster, too slow. Maybe check as we go?
-    println!("combos = {:?}", combos.len());
+    let min_required = sizes.iter().sum::<i32>() + sizes.len() as i32 - 1;
+    let to_allocate = str.len() as i32 - min_required;
+    let first_spring = str.find('#').unwrap_or(str.len()) as i32;
+    let check_to = min(to_allocate, first_spring) as usize;
+    let expected = sizes[0] as usize;
 
-    let chr = str.as_bytes();
+    let b = str.as_bytes();
     let mut arrangements = 0;
-    'outer: for c in combos {
-        let mut pos = 0;
-        let mut valid = true;
-        assert_eq!(c.len(), sizes.len() + 1);
-
-        for i in 0..sizes.len() {
-            let expected = if i == 0 { c[i] } else { c[i] + 1 };
-            for j in 0..expected {
-                if chr[pos] != b'.' && chr[pos] != b'?' {
-                    valid = false;
-                    continue 'outer;
-                }
-                pos += 1;
-            }
-            for j in 0..sizes[i] {
-                if chr[pos] != b'#' && chr[pos] != b'?' {
-                    valid = false;
-                    continue 'outer;
-                }
-                pos += 1;
-            }
-        }
-        for i in 0..*c.last().unwrap() {
-            if chr[pos] != b'.' && chr[pos] != b'?' {
-                valid = false;
-                continue 'outer;
-            }
-            pos += 1;
-        }
-        println!("{:?} is {valid}", c);
-        if valid {
-            for i in 0..sizes.len() {
-                for j in 0..c[i] {
-                    print!(".");
-                }
-                for j in 0..sizes[i] {
-                    print!("#");
-                }
-            }
-            for j in 0..*c.last().unwrap() {
-                print!(".");
-            }
-            println!();
-            arrangements += 1;
+    for i in min_space..=check_to {
+        if b[0..i].iter().all(|c| *c == b'.' || *c == b'?')
+            && b[i..i + expected].iter().all(|c| *c == b'#' || *c == b'?')
+        {
+            arrangements += find_arrangements_internal(&str[i + expected..], &sizes[1..], 1);
         }
     }
-
-    println!("Found {arrangements}");
 
     arrangements
 }
