f85bde8aa2cb6f500c42cd367cc3493d57426d39
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-13 17:07:10 +1100

    day 13, part 2 first submission

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -24,7 +24,7 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
             if same {
                 total += (idx + 1) * 100;
-                break;
+                break; // TODO: skip vertical
             }
         }
 
@@ -50,7 +50,55 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let p = parser!(sections(
+        lines(char_of(".#")+)
+    ));
+    let v = p.parse(input).unwrap();
+
+    let mut total = 0;
+    for section in v {
+        println!("Check section");
+        // horizontal
+        for idx in 0..section.len() - 1 {
+            let num = min(idx + 1, section.len() - idx - 1);
+            let mut errors = 0;
+            for i in 0..num {
+                let (line1, line2) = (&section[idx - i], &section[idx + (i + 1)]);
+                errors += (0..line1.len()).filter(|j| line1[*j] != line2[*j]).count();
+                if errors > 1 {
+                    break;
+                }
+            }
+            if errors == 1 {
+                println!("Horizontal {}", idx + 1);
+                total += (idx + 1) * 100;
+                break; // TODO: skip vertical
+            }
+        }
+
+        // vertical
+        // TODO: refactor
+        for idx in 0..section[0].len() - 1 {
+            let num = min(idx + 1, section[0].len() - idx - 1);
+            let mut errors = 0;
+            for i in 0..num {
+                errors += section
+                    .iter()
+                    .filter(|row| row[idx - i] != row[idx + (i + 1)])
+                    .count();
+                if errors > 1 {
+                    break;
+                }
+            }
+            if errors == 1 {
+                println!("Vertical {}", idx + 1);
+                total += idx + 1;
+                break;
+            }
+        }
+    }
+
+    Some(total as u32)
 }
 
 #[cfg(test)]
@@ -66,6 +114,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400));
     }
 }
