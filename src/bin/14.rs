c6e798a47548d1e7b58533638678dff6187bdd16
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-14 23:38:40 +1100

    clean up day 14 code

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 
 use aoc_parse::{parser, prelude::*};
-use itertools::Itertools;
-use seahash::{hash, SeaHasher};
+use itertools::{Either, Itertools};
+use seahash::hash;
 
 advent_of_code::solution!(14);
 
@@ -10,188 +10,169 @@ const EMPTY: u8 = 0;
 const SQUARE: u8 = 1;
 const ROUND: u8 = 2;
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let p = parser!(lines(
-        char_of(".#O")+
-    ));
+enum Direction {
+    N,
+    W,
+    S,
+    E,
+}
 
-    let mut grid = p.parse(input).unwrap();
-    // let direction = (0, -1);
+struct Grid {
+    w: i32,
+    h: i32,
+    grid: Vec<u8>,
+}
 
-    let w = grid[0].len();
-    let h = grid.len();
-    let mut last_free = vec![0; w];
+impl Grid {
+    fn rock_and_roll(&mut self, direction: Direction) {
+        // roll along grid height for N/S, grid width for E/W
+        // the other axis is what we scan across to process for each roll step
+        let (scan_length, roll_length) = match direction {
+            Direction::N | Direction::S => (self.w, self.h),
+            Direction::E | Direction::W => (self.h, self.w),
+        };
 
-    // TODO: this is direction dependent...
-    for y in 0..h {
-        for x in 0..w {
-            if grid[y][x] == SQUARE as usize {
-                last_free[x] = y + 1;
+        // We step along from the destination of the roll, so
+        // N/W start at 0 and move positively, S/E start at end and move negavtively
+        let (start_value, inc) = match direction {
+            Direction::N | Direction::W => (0, 1),
+            Direction::S | Direction::E => (roll_length - 1, -1),
+        };
+
+        // Find the index into the byte array based on the direction
+        fn get_index(direction: &Direction, size: i32, scan_idx: i32, roll_idx: i32) -> usize {
+            match direction {
+                Direction::N | Direction::S => (roll_idx * size + scan_idx) as usize,
+                Direction::E | Direction::W => (scan_idx * size + roll_idx) as usize,
             }
-            if grid[y][x] == ROUND as usize {
-                if last_free[x] < y {
-                    // move up
-                    grid[last_free[x]][x] = ROUND as usize;
-                    grid[y][x] = EMPTY as usize;
-                    last_free[x] += 1;
-                } else {
-                    last_free[x] = y + 1;
+        }
+
+        // Create a range iterator based on the direction we are rolling
+        fn get_range(inc: i32, roll_length: i32) -> impl Iterator<Item = i32> {
+            if inc > 0 {
+                Either::Left(0..roll_length)
+            } else {
+                Either::Right((0..roll_length).rev())
+            }
+        }
+
+        // Keep track of the point a round rock could roll to
+        let mut last_free = vec![start_value; scan_length as usize];
+
+        // Start at the destination for each column and:
+        //  if we find an obstacle, set that as the new roll destination for any rocks found
+        //  if we find a round rock, roll it to the roll destination and set the new destination above it
+        //  if we find a round rock, but nowhere for it to roll to, just set the new destination above it
+        for roll_idx in get_range(inc, roll_length) {
+            for scan_idx in 0..scan_length {
+                let grid_idx = get_index(&direction, scan_length, scan_idx, roll_idx);
+                if self.grid[grid_idx] == SQUARE {
+                    last_free[scan_idx as usize] = roll_idx + inc;
+                }
+                if self.grid[grid_idx] == ROUND {
+                    if (inc > 0 && last_free[scan_idx as usize] < roll_idx)
+                        || (inc < 0 && last_free[scan_idx as usize] > roll_idx)
+                    {
+                        self.grid[get_index(
+                            &direction,
+                            scan_length,
+                            scan_idx,
+                            last_free[scan_idx as usize],
+                        )] = ROUND;
+                        self.grid[grid_idx] = EMPTY;
+                        last_free[scan_idx as usize] = last_free[scan_idx as usize] + inc;
+                    } else {
+                        last_free[scan_idx as usize] = roll_idx + inc;
+                    }
                 }
             }
         }
     }
 
-    let count = grid
-        .iter()
-        .enumerate()
-        .map(|(y, row)| row.iter().filter(|r| **r == ROUND as usize).count() * (h - y))
-        .sum::<usize>();
-    Some(count as u32)
+    fn calculate_load(&self) -> i32 {
+        // For each round rock, find the line it is on (idx / w)
+        // and calculate the number of lines from north (h - value)
+        // Return the sum of all rocks
+        self.grid
+            .iter()
+            .enumerate()
+            .map(|(idx, v)| {
+                if *v == ROUND {
+                    self.h - idx as i32 / self.w
+                } else {
+                    0
+                }
+            })
+            .sum::<i32>()
+    }
+
+    fn _print_grid(&self) {
+        for row in 0..self.h {
+            for col in 0..self.w {
+                print!(
+                    "{}",
+                    match self.grid[(row * self.w + col) as usize] {
+                        EMPTY => ".",
+                        ROUND => "O",
+                        SQUARE => "#",
+                        _ => "?",
+                    }
+                );
+            }
+            println!();
+        }
+        println!();
+    }
+
+    fn parse_grid(input: &str) -> Self {
+        let p = parser!(lines(
+            char_of(".#O")+
+        ));
+
+        let map = p.parse(input).unwrap();
+
+        Self {
+            w: map[0].len() as i32,
+            h: map.len() as i32,
+            grid: map.concat().into_iter().map(|b| b as u8).collect_vec(), // convert into a byte array
+        }
+    }
+}
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut grid = Grid::parse_grid(input);
+    grid.rock_and_roll(Direction::N);
+    Some(grid.calculate_load() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let p = parser!(lines(
-        char_of(".#O")+
-    ));
-
-    let map = p.parse(input).unwrap();
-    // let direction = (0, -1);
-
-    let w = map[0].len();
-    let h = map.len();
-
-    let mut grid = map.concat().into_iter().map(|b| b as u8).collect_vec();
+    let mut grid = Grid::parse_grid(input);
     let mut seen = HashMap::new();
-
-    print_grid(&grid, w, h);
 
     let mut target = 1000000000;
     let mut num = 0;
     while num < target {
-        let k = hash(grid.as_slice());
+        // hash the current grid as a byte array and store in a hashmap
+        let k = hash(grid.grid.as_slice());
         if let Some(prev) = seen.insert(k, num) {
+            // If we found one that was previously encountered, there is a cycle
+            // Measure the interval of the cycle
             let interval = num - prev;
-            let end = (1000000000 - prev) % interval;
-            println!("Cycle {} {} -> {} {}", prev, num, end, prev + end);
-            target = num + end;
+            // Find how many iterations would be left to do if we repeated this
+            // cycle to the last one before the target
+            let remaining = (target - num) % interval;
+            // set the target to the same point within the current cycle to short circuit
+            target = num + remaining;
         }
 
-        let mut last_free = vec![0; w];
+        grid.rock_and_roll(Direction::N);
+        grid.rock_and_roll(Direction::W);
+        grid.rock_and_roll(Direction::S);
+        grid.rock_and_roll(Direction::E);
 
-        // TODO: this is direction dependent...
-        for y in 0..h {
-            for x in 0..w {
-                if grid[y * w + x] == SQUARE {
-                    last_free[x] = y + 1;
-                }
-                if grid[y * w + x] == ROUND {
-                    if last_free[x] < y {
-                        // move up
-                        grid[last_free[x] * w + x] = ROUND;
-                        grid[y * w + x] = EMPTY;
-                        last_free[x] += 1;
-                    } else {
-                        last_free[x] = y + 1;
-                    }
-                }
-            }
-        }
-
-        let mut last_free = vec![0; h];
-
-        // TODO: this is direction dependent...
-        for x in 0..w {
-            for y in 0..h {
-                if grid[y * w + x] == SQUARE {
-                    last_free[y] = x + 1;
-                }
-                if grid[y * w + x] == ROUND {
-                    if last_free[y] < x {
-                        // move west
-                        grid[y * w + last_free[y]] = ROUND;
-                        grid[y * w + x] = EMPTY;
-                        last_free[y] += 1;
-                    } else {
-                        last_free[y] = x + 1;
-                    }
-                }
-            }
-        }
-        let mut last_free = vec![h - 1; w];
-
-        // TODO: this is direction dependent...
-        for y in (0..h).rev() {
-            for x in 0..w {
-                if grid[y * w + x] == SQUARE {
-                    if y > 0 {
-                        last_free[x] = y - 1;
-                    }
-                }
-                if grid[y * w + x] == ROUND {
-                    if last_free[x] > y {
-                        // move south
-                        grid[last_free[x] * w + x] = ROUND;
-                        grid[y * w + x] = EMPTY;
-                        last_free[x] -= 1;
-                    } else {
-                        if y > 0 {
-                            last_free[x] = y - 1;
-                        }
-                    }
-                }
-            }
-        }
-
-        let mut last_free = vec![w - 1; h];
-
-        // TODO: this is direction dependent...
-        for x in (0..w).rev() {
-            for y in 0..h {
-                if grid[y * w + x] == SQUARE {
-                    if x > 0 {
-                        last_free[y] = x - 1;
-                    }
-                }
-                if grid[y * w + x] == ROUND {
-                    if last_free[y] > x {
-                        // move east
-                        grid[y * w + last_free[y]] = ROUND;
-                        grid[y * w + x] = EMPTY;
-                        last_free[y] -= 1;
-                    } else {
-                        if x > 0 {
-                            last_free[y] = x - 1;
-                        }
-                    }
-                }
-            }
-        }
         num += 1;
     }
-    let count = grid
-        .iter()
-        .enumerate()
-        .map(|(idx, v)| if *v == ROUND { h - idx / w } else { 0 })
-        .sum::<usize>();
-    Some(count as u32)
-}
-
-fn print_grid(grid: &Vec<u8>, w: usize, h: usize) {
-    for row in 0..h {
-        for col in 0..w {
-            print!(
-                "{}",
-                match grid[row * w + col] {
-                    EMPTY => ".",
-                    ROUND => "O",
-                    SQUARE => "#",
-                    _ => "?",
-                }
-            );
-        }
-        println!();
-    }
-    println!();
+    Some(grid.calculate_load() as u32)
 }
 
 #[cfg(test)]
