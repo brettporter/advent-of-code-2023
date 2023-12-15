7bb5eab1041e1b0cd2903fabc84dd092a19f7174
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-15 17:32:59 +1100

    day 15, part 2 first submission

--- src/bin/15.rs
+++ src/bin/15.rs
@@ -1,3 +1,5 @@
+use linked_hash_map::LinkedHashMap;
+
 advent_of_code::solution!(15);
 
 fn hash_string(s: &str) -> u8 {
@@ -15,7 +17,37 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut boxes = vec![LinkedHashMap::new(); 256];
+
+    for s in input.trim().split(',') {
+        let v: Vec<&str> = s.split(&['-', '=']).collect();
+        let (label, rest) = (v[0], v[1]);
+        let b = hash_string(label);
+
+        match s.chars().nth(label.len()).unwrap() {
+            '-' => {
+                boxes[b as usize].remove(label);
+            }
+            '=' => {
+                let focal_length = rest[..].parse::<u8>().unwrap();
+                if !boxes[b as usize].contains_key(label) {
+                    boxes[b as usize].insert(label, focal_length);
+                } else {
+                    boxes[b as usize][label] = focal_length;
+                }
+            }
+            _ => panic!("Invalid character {}", s),
+        }
+    }
+
+    let mut total = 0;
+    for (box_num, b) in boxes.iter().enumerate() {
+        for (pos, (_, value)) in b.iter().enumerate() {
+            let focusing_power = (1 + box_num) * (pos + 1) * *value as usize;
+            total += focusing_power;
+        }
+    }
+    Some(total as u32)
 }
 
 #[cfg(test)]
@@ -36,6 +68,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(145));
     }
 }
