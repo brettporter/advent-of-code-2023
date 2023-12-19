34b86162eb666350a0e40d84dd819ca9fd6eb2d4
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-19 18:31:57 +1100

    Day 19, part 2 first submission

--- src/bin/19.rs
+++ src/bin/19.rs
@@ -6,7 +6,7 @@ advent_of_code::solution!(19);
 
 #[derive(Debug)]
 enum WorkflowRule {
-    Rule(usize, usize, i32, String),
+    Rule(usize, usize, usize, String),
     Command(String),
 }
 
@@ -16,7 +16,7 @@ const GT: usize = 1;
 fn execute_workflow(
     name: String,
     workflows: &HashMap<String, Vec<WorkflowRule>>,
-    part: &Vec<i32>,
+    part: &Vec<usize>,
 ) -> bool {
     if name == "R" {
         return false;
@@ -40,14 +40,18 @@ fn execute_workflow(
             WorkflowRule::Command(cmd) => return execute_workflow(cmd.to_owned(), workflows, part),
         }
     }
-    // Accepted if reach end
-    true
+    unreachable!()
 }
 
-pub fn part_one(input: &str) -> Option<i32> {
+fn parse(
+    input: &str,
+) -> (
+    Vec<(usize, usize, usize, usize)>,
+    HashMap<String, Vec<WorkflowRule>>,
+) {
     let workflow_rule = parser!(
         {
-            cat:char_of("xmas") op:char_of("<>") value:i32 ":" workflow:string(alpha+) => WorkflowRule::Rule(cat, op, value, workflow),
+            cat:char_of("xmas") op:char_of("<>") value:usize ":" workflow:string(alpha+) => WorkflowRule::Rule(cat, op, value, workflow),
             workflow:string(alpha+) => WorkflowRule::Command(workflow),
         }
     );
@@ -60,7 +64,7 @@ pub fn part_one(input: &str) -> Option<i32> {
         )
         section(
             lines(
-                "{x=" i32 ",m=" i32 ",a=" i32 ",s=" i32 "}" // Currently assuming all present - can do char_of("xmas") and map later if not
+                "{x=" usize ",m=" usize ",a=" usize ",s=" usize "}" // Currently assuming all present - can do char_of("xmas") and map later if not
             )
         )
     );
@@ -71,6 +75,53 @@ pub fn part_one(input: &str) -> Option<i32> {
     for (name, rules) in workflows {
         workflow_map.insert(name, rules);
     }
+    (parts, workflow_map)
+}
+
+fn count_accepted_combinations(
+    segments: Vec<(usize, usize)>,
+    name: String,
+    workflow_map: &HashMap<String, Vec<WorkflowRule>>,
+) -> usize {
+    if name == "R" {
+        return 0;
+    }
+    if name == "A" {
+        return segments
+            .iter()
+            .map(|s| s.1 - s.0 + 1)
+            .reduce(|acc, e| acc * e)
+            .unwrap();
+    }
+
+    let mut total = 0;
+    let mut remaining = segments;
+    for rule in workflow_map.get(&name).unwrap() {
+        match rule {
+            WorkflowRule::Rule(cat, op, value, workflow) => {
+                let (wf_seg, rem_seg) = match *op {
+                    LT => ((remaining[*cat].0, *value - 1), (*value, remaining[*cat].1)),
+                    GT => ((*value + 1, remaining[*cat].1), (remaining[*cat].0, *value)),
+                    _ => unreachable!(),
+                };
+                let mut wf = remaining.clone();
+                wf[*cat] = wf_seg;
+                total += count_accepted_combinations(wf, workflow.to_owned(), workflow_map);
+
+                remaining = remaining.clone();
+                remaining[*cat] = rem_seg;
+            }
+            WorkflowRule::Command(cmd) => {
+                return total + count_accepted_combinations(remaining, cmd.to_owned(), workflow_map)
+            }
+        }
+    }
+
+    unreachable!()
+}
+
+pub fn part_one(input: &str) -> Option<usize> {
+    let (parts, workflow_map) = parse(input);
 
     let mut accepted = Vec::new();
 
@@ -81,11 +132,19 @@ pub fn part_one(input: &str) -> Option<i32> {
         }
     }
 
-    Some(accepted.iter().map(|part| part.iter().sum::<i32>()).sum())
+    Some(accepted.iter().map(|part| part.iter().sum::<usize>()).sum())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<usize> {
+    let (_, workflow_map) = parse(input);
+
+    let segments = vec![(1, 4000); 4];
+
+    Some(count_accepted_combinations(
+        segments,
+        String::from("in"),
+        &workflow_map,
+    ))
 }
 
 #[cfg(test)]
@@ -101,6 +160,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(167409079868000));
     }
 }
