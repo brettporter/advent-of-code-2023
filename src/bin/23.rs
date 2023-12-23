5602114e833403596f2062f07decc3742775ef7f
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-23 17:51:44 +1100

    Day 23, part 2 - brute force

--- src/bin/23.rs
+++ src/bin/23.rs
@@ -68,8 +68,120 @@ pub fn part_one(input: &str) -> Option<usize> {
     valid_paths.iter().map(|p| p.len() - 1).max()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    let grid = input
+        .trim()
+        .split("\n")
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+
+    let (w, h) = (grid[0].len(), grid.len());
+    let start = (1, 0);
+    let dest = (w - 2, h - 1);
+
+    let visited = FxHashSet::default();
+
+    let mut valid_paths = Vec::new();
+    let mut queue = VecDeque::new();
+    queue.push_back((start, start, 0, visited));
+
+    let mut max_Distance = 0;
+
+    // TODO: alternative could be to just search for junction points and search tree
+    while let Some(((x, y), from, total_distance, visited)) = queue.pop_front() {
+        // println!("Following path from junction {} {}", x, y);
+        if (x, y) == dest {
+            if total_distance - 1 > max_Distance {
+                println!(
+                    "Found dest {} - remaining {}",
+                    total_distance - 1,
+                    queue.len()
+                );
+                max_Distance = total_distance - 1;
+            }
+            valid_paths.push(total_distance - 1);
+            continue;
+        }
+
+        let mut from = from;
+        let mut cur = (x, y);
+        let mut distance = 0;
+
+        loop {
+            distance += 1;
+            let (x, y) = cur;
+            if (x, y) == dest {
+                if distance + total_distance - 1 > max_Distance {
+                    println!(
+                        "Found dest {} - remaining {}",
+                        distance + total_distance - 1,
+                        queue.len()
+                    );
+                    max_Distance = distance + total_distance - 1;
+                }
+                valid_paths.push(distance + total_distance - 1);
+                break;
+            }
+
+            let mut choices = Vec::new();
+            // left
+            if x > 0 {
+                if grid[y][x - 1] != '#' {
+                    if from != (x - 1, y) {
+                        choices.push((x - 1, y));
+                    }
+                }
+            }
+            // right
+            if x < w - 1 {
+                if grid[y][x + 1] != '#' {
+                    if from != (x + 1, y) {
+                        choices.push((x + 1, y));
+                    }
+                }
+            }
+            // up
+            if y > 0 {
+                if grid[y - 1][x] != '#' {
+                    if from != (x, y - 1) {
+                        choices.push((x, y - 1));
+                    }
+                }
+            }
+            // down
+            if y < h - 1 {
+                if grid[y + 1][x] != '#' {
+                    if from != (x, y + 1) {
+                        choices.push((x, y + 1));
+                    }
+                }
+            }
+
+            if choices.len() == 0 {
+                break;
+            }
+            if choices.len() == 1 {
+                from = (x, y);
+                cur = *choices.first().unwrap();
+                continue;
+            }
+
+            // Junction point
+            // println!("Found junction {} {} -> {:?}", x, y, choices);
+            if visited.contains(&(x, y)) {
+                break;
+            }
+
+            let mut visited = visited.clone();
+            visited.insert((x, y));
+            for n in choices {
+                queue.push_back((n, (x, y), total_distance + distance, visited.clone()));
+            }
+            break;
+        }
+    }
+
+    Some(*valid_paths.iter().max().unwrap() as i32)
 }
 
 #[cfg(test)]
@@ -85,6 +197,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(154));
     }
 }
