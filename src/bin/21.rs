aa11ffc822bd33e9b4fb4578bf4a159d7b9061fe
Brett Porter <49108+brettporter@users.noreply.github.com>
2023-12-24 00:53:23 +1100

    solution for day 21 part 2 that works for input

--- src/bin/21.rs
+++ src/bin/21.rs
@@ -1,10 +1,8 @@
 use std::collections::HashSet;
 
-use itertools::Itertools;
-
 advent_of_code::solution!(21);
 
-fn count_destinations(input: &str, num_steps: u32) -> Option<u32> {
+fn count_destinations(input: &str, num_steps: u32) -> Option<usize> {
     let lines = input.trim().split("\n").collect::<Vec<_>>();
     let size = lines.len();
     assert_eq!(lines[0].len(), size);
@@ -12,32 +10,23 @@ fn count_destinations(input: &str, num_steps: u32) -> Option<u32> {
     let mut grid = vec![vec![0; size]; size];
     let mut start = None;
 
-    let mut num_rocks = 0;
-
     for (y, line) in lines.iter().enumerate() {
         for (x, c) in line.chars().enumerate() {
             match c {
                 '.' => (),
-                '#' => {
-                    num_rocks += 1;
-                    grid[y][x] = 1
-                }
+                '#' => grid[y][x] = 1,
                 'S' => start = Some((y as i32, x as i32)),
                 _ => panic!("Invalid input"),
             }
         }
     }
 
-    println!("Rocks = {num_rocks}");
-
     let mut locations = HashSet::new();
-    let mut locations_len = 1;
 
-    let mut full_grid = 0;
-    // let mut next_cube = 2;
+    const GARDEN_SIZE: usize = 7;
 
     locations.insert(start.unwrap());
-    for i in 0..num_steps {
+    for _ in 0..num_steps {
         let mut new_locations = HashSet::new();
         let mut done = false;
         for (x, y) in locations {
@@ -46,7 +35,7 @@ fn count_destinations(input: &str, num_steps: u32) -> Option<u32> {
                 == 0
             {
                 new_locations.insert((x - 1, y));
-                if x == -((GARDEN_SIZE / 2) * size as i32) + 1 {
+                if x == -((GARDEN_SIZE / 2) as i32 * size as i32) + 1 {
                     done = true;
                 }
             }
@@ -55,7 +44,7 @@ fn count_destinations(input: &str, num_steps: u32) -> Option<u32> {
                 == 0
             {
                 new_locations.insert((x, y - 1));
-                if y == -((GARDEN_SIZE / 2) * size as i32) + 1 {
+                if y == -((GARDEN_SIZE / 2) as i32 * size as i32) + 1 {
                     done = true;
                 }
             }
@@ -64,7 +53,7 @@ fn count_destinations(input: &str, num_steps: u32) -> Option<u32> {
                 == 0
             {
                 new_locations.insert((x + 1, y));
-                if x == size as i32 * (GARDEN_SIZE / 2 + 1) - 1 {
+                if x == size as i32 * (GARDEN_SIZE / 2 + 1) as i32 - 1 {
                     done = true;
                 }
             }
@@ -73,106 +62,65 @@ fn count_destinations(input: &str, num_steps: u32) -> Option<u32> {
                 == 0
             {
                 new_locations.insert((x, y + 1));
-                if y == size as i32 * (GARDEN_SIZE / 2 + 1) - 1 {
+                if y == size as i32 * (GARDEN_SIZE / 2 + 1) as i32 - 1 {
                     done = true;
                 }
             }
         }
-        println!(
-            "Step {}: Total nodes {}, Added Nodes {}",
-            i + 1,
-            new_locations.len(),
-            new_locations.len() - locations_len,
-        );
         locations = new_locations;
-        locations_len = locations.len();
         if done {
-            break;
-        }
-        if locations.contains(&(-(size as i32 * full_grid), -(size as i32 * full_grid)))
-            && locations.contains(&(
-                -(size as i32 * full_grid),
-                (size as i32 * (full_grid + 1)) - 1,
-            ))
-            && locations.contains(&(
-                (size as i32 * (full_grid + 1)) - 1,
-                -(size as i32 * full_grid),
-            ))
-            && locations.contains(&(
-                (size as i32 * (full_grid + 1)) - 1,
-                (size as i32 * (full_grid + 1)) - 1,
-            ))
-        {
-            println!(
-                "Total nodes {}, Central Nodes {}",
-                locations.len(),
-                locations
-                    .iter()
-                    .filter(|(x, y)| *x >= 0 && *y >= 0 && *x < size as i32 && *y < size as i32)
-                    .count()
-            );
-            println!(
-                "Found {} count {} step {}/{}",
-                full_grid,
-                locations.len(),
-                i,
-                num_steps
-            );
-            let mmx = locations.iter().minmax_by_key(|k| k.0);
-            let mmy = locations.iter().minmax_by_key(|k| k.1);
-            println!("Dim {:?} {:?}", mmx, mmy);
-            full_grid += 1;
-        }
+            let mut garden_count = vec![vec![0; GARDEN_SIZE as usize]; GARDEN_SIZE as usize];
+            let offset = size as i32 * (GARDEN_SIZE / 2) as i32;
+            for y in 0..size * GARDEN_SIZE {
+                for x in 0..size * GARDEN_SIZE {
+                    if locations.contains(&((x as i32 - offset), (y as i32 - offset))) {
+                        garden_count[y / size][x / size] += 1;
+                    }
+                }
+            }
 
-        // if locations.len() >= (next_cube * next_cube) * ((next_cube + 1) * (next_cube + 1)) / 4 {
-        //     // TODO: check size?
-        //     println!(
-        //         "Found cube {} step {} count {}",
-        //         next_cube,
-        //         i,
-        //         locations.len()
-        //     );
-        //     next_cube += 1;
-        // }
+            // TODO: assumption here of constant being the right one
+            let n = num_steps as usize / size;
+            let total = n
+                * (garden_count[1][1]
+                    + garden_count[5][1]
+                    + garden_count[1][5]
+                    + garden_count[5][5])
+                + (n - 1)
+                    * (garden_count[1][2]
+                        + garden_count[1][4]
+                        + garden_count[5][2]
+                        + garden_count[5][4])
+                + garden_count[0][3]
+                + garden_count[3][0]
+                + garden_count[3][6]
+                + garden_count[6][3]
+                + n * n * garden_count[1][3]
+                + (n - 1) * (n - 1) * garden_count[2][3];
+            return Some(total);
+        }
     }
-    _print_locations(&locations, &grid);
-    println!();
 
-    Some(locations.len() as u32)
+    Some(locations.len())
 }
 
-const GARDEN_SIZE: i32 = 7;
-
 fn _print_locations(locations: &HashSet<(i32, i32)>, grid: &Vec<Vec<i32>>) {
+    const GARDEN_SIZE: i32 = 3;
+
     let (w, h) = (grid[0].len() as i32, grid.len() as i32);
-    let mut garden_count = vec![vec![0; GARDEN_SIZE as usize]; GARDEN_SIZE as usize];
     for y in -h * (GARDEN_SIZE / 2)..h * (GARDEN_SIZE / 2 + 1) {
         for x in -w * (GARDEN_SIZE / 2)..w * (GARDEN_SIZE / 2 + 1) {
             if locations.contains(&(x as i32, y as i32)) {
-                garden_count[((y + h * (GARDEN_SIZE / 2)) / h) as usize]
-                    [((x + w * (GARDEN_SIZE / 2)) / w) as usize] += 1;
-                // print!("O");
+                print!("O");
             } else {
-                // print!(
-                //     "{}",
-                //     match grid[y.rem_euclid(h) as usize][x.rem_euclid(w) as usize] {
-                //         0 => ".",
-                //         1 => "#",
-                //         _ => panic!(),
-                //     }
-                // );
-            }
-        }
-        println!();
-    }
-    println!();
-
-    for (y, row) in garden_count.iter().enumerate() {
-        for (x, col) in row.iter().enumerate() {
-            if *col == 0 {
-                print!("    ");
-            } else {
-                print!("{:6}", col);
+                print!(
+                    "{}",
+                    match grid[y.rem_euclid(h) as usize][x.rem_euclid(w) as usize] {
+                        0 => ".",
+                        1 => "#",
+                        _ => panic!(),
+                    }
+                );
             }
         }
         println!();
@@ -180,11 +128,11 @@ fn _print_locations(locations: &HashSet<(i32, i32)>, grid: &Vec<Vec<i32>>) {
     println!();
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     count_destinations(input, 64)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<usize> {
     count_destinations(input, 26501365)
 }
 
